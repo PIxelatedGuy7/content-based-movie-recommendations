09255bbd6fab9ba2b76a668f0973c2459ba4363c
PIxelatedGuy7 <madhavharshal7@gmail.com>
2025-08-08 23:48:08 +0530

    Update movieRecommender.py

--- movieRecommender.py
+++ movieRecommender.py
@@ -25,8 +25,9 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('/movie_list.pkl','rb'))
-similarity = pickle.load(open('/similarity.pkl','rb'))
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+
+similarity = pickle.load(open('similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
@@ -53,6 +54,7 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
+
 
 
 
